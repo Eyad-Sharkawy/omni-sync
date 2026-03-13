b0e57d68ed3cd312dd258ac33c2ecdcf5330e244
Eyad-Sharkawy <eyad.m.sharkawy@gmail.com>
2026-03-13 14:52:36 +0200

    feat: added the initial nav bar component of the app

---
+++ src/app/layout/navbar/navbar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'os-navbar',
+  imports: [],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css',
+})
+export class Navbar {
+
+}
