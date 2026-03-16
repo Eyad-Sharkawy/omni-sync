bf38e7c2f903471ba2ca1f2c54f41554e503aae9
Eyad-Sharkawy <eyad.m.sharkawy@gmail.com>
2026-03-16 12:14:42 +0200

    Refactor: Changed the navbar html for seo optimization

--- src/app/layout/navbar/navbar.ts
+++ src/app/layout/navbar/navbar.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'os-navbar',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
