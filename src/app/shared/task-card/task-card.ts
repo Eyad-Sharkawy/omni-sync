a0ad5293f4495df398f96bac2f4d13692bf85bb3
Eyad-Sharkawy <eyad.m.sharkawy@gmail.com>
2026-03-16 12:18:51 +0200

    Feat: added the initial task card shared component

---
+++ src/app/shared/task-card/task-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'os-task-card',
+  imports: [],
+  templateUrl: './task-card.html',
+  styleUrl: './task-card.css',
+})
+export class TaskCard {
+
+}
