4cd4e87c8076157f51160813b69ec0fa9b4d7675
Eyad-Sharkawy <eyad.m.sharkawy@gmail.com>
2026-03-16 14:04:44 +0200

    refactor: changed the shell style to use tailwind style classes instead of default css styling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,13 @@
 import { Component, signal } from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 import {Navbar} from './layout/navbar/navbar';
-import {TasksColumn} from './shared/tasks-column/tasks-column';
-import {TaskCard} from './shared/task-card/task-card';
-import {Kanban} from './features/kanban/kanban';
-import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'os-root',
-  imports: [Navbar, TasksColumn, TaskCard, Kanban, RouterOutlet],
+  imports: [Navbar, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('omni-sync');
 }
