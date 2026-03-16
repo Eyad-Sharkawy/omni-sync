e2263378a99d65743aaea4f4613fd343912a457a
Eyad-Sharkawy <eyad.m.sharkawy@gmail.com>
2026-03-16 12:17:23 +0200

    Fix: Fixed Tailwind Classes not being added due to Tailwind's JIT compiler not being able to generate the classes in advance

--- src/app/shared/tasks-column/tasks-column.ts
+++ src/app/shared/tasks-column/tasks-column.ts
@@ -1,4 +1,4 @@
-import {Component, input} from '@angular/core';
+import {Component, computed, input, numberAttribute} from '@angular/core';
 
 type columnTypes = "todo" | "inprog" | "review" | "done";
 
@@ -8,9 +8,30 @@ type columnTypes = "todo" | "inprog" | "review" | "done";
   templateUrl: './tasks-column.html',
   styleUrl: './tasks-column.css',
   host: {
-    "[class]": "'before:bg-' + columnType()"
+    "[class]": "hostClass()"
   }
 })
 export class TasksColumn {
+  numberOfTasks = input.required({transform: numberAttribute});
   columnType = input.required<columnTypes>()
+
+  protected hostClass = computed(() => {
+    const type = this.columnType();
+
+    const classes: Record<columnTypes, string> = {
+      todo: "before:bg-todo",
+      inprog: "before:bg-inprog",
+      review: "before:bg-review",
+      done: "before:bg-done",
+    };
+
+    return classes[type];
+  });
+
+  headerSpanClasses: Record<columnTypes, string> = {
+    todo: "bg-todo shadow-todo",
+    inprog: "bg-inprog shadow-inprog",
+    review: "bg-review shadow-review",
+    done: "bg-done shadow-done",
+  }
 }
