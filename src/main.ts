0350e1d8167f91ef2eae6f5c03222a30957be5f2
Eyad-Sharkawy <eyad.m.sharkawy@gmail.com>
2026-03-27 04:13:57 +0200

    feat: added vercel analytics and vercel speed insights

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { bootstrapApplication } from "@angular/platform-browser";
 import { appConfig } from "./app/app.config";
+
+import { inject } from "@vercel/analytics";
+import { injectSpeedInsights } from "@vercel/speed-insights";
+
 import { App } from "./app/app";
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
+
+inject();
+injectSpeedInsights();
